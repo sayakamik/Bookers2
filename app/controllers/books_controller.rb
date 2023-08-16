7bba421bb8897b558aa3d44399b5c1810b81396b
sayakamik <138635063+sayakamik@users.noreply.github.com>
2023-08-16 13:39:47 +0000

    [Initial Commit2] Bookers3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,20 +5,40 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(current_user.id)
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
   end
 
   def index
+    @user = User.find(current_user.id)
+    @users = User.all
     @books= Book.all
     @book= Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
+    @book1 = Book.new
+    @user = @book.user
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @books = Book.all
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+  end
+
+  def destroy
+    @book = Book.find(params[:id])  # データ（レコード）を1件取得
+    @book.destroy  # データ（レコード）を削除
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,25 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    #@books = @user.books
+    @users = User.all
+    @books = Book.all
+    @book = Book.new
+
   end
 
   def edit
-    @user = User.find
-
+    @user = User.find(params[:id])
   end
+
+  def index
+    @user = User.find(params[:id])
+    @users = User.all
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def book_params
+    params.require(:book).permit(:image, :title, :body)
+  end
+
 end
